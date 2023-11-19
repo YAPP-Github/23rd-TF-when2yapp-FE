1d350da52495f17771fde2ec8151f290e99f0f86
Haeseong Jeon <seong0428@gmail.com>
2023-11-19 20:46:56 +0900

    feat: title table layout 추가

--- lib/schedule_detail_page.dart
+++ lib/schedule_detail_page.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
+import 'component/when2yapp_time_table.dart';
+
 class ScheduleDetailPage extends StatelessWidget {
+  final int numberOfPeople = 7;
+  final DateTime scheduleStartDate = DateTime.now();
+  final DateTime scheduleEndDate = DateTime.now().add(const Duration(days: 4));
+  final int scheduleStartTime = 7;
+  final int scheduleEndTime = 26;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(),
-        body: Center(
-          child: Text('7명의 만날 수 있는 시간이에요.'),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('$numberOfPeople명의 만날 수 있는 시간이에요.'),
+              When2YappTimeTable(
+                startDate: scheduleStartDate,
+                endDate: scheduleEndDate,
+              ),
+            ],
+          ),
         ),
         floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
