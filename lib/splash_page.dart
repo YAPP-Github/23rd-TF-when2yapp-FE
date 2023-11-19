2a794864ae46a119b64d2871996efc432c519573
Jihee.Han <jihee.dev@gmail.com>
2023-11-20 01:45:31 +0900

    feat: 메인 화면(splash) 디자인 구현 (#18)
    Co-authored-by: Jihee Han <jihee.han@vinulabs.com>

--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -4,16 +4,66 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('우리 도대체\n만나는 날짜가\n언제 YAPP'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).pushNamed('/create');
-        },
-        tooltip: '약속 만들기',
-        child: const Icon(Icons.add),
+      backgroundColor: const Color(0xFFF5F5F8),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+              margin: const EdgeInsets.fromLTRB(0, 40, 0, 130),
+              width: double.infinity,
+              child: Image.asset('assets/splashbackground.png')),
+          Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.fromLTRB(30, 70, 0, 0),
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  '우리 도대체\n만나는 날짜가',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 38,
+                      color: Colors.black),
+                ),
+              ),
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(30, 2, 0, 0),
+                    alignment: Alignment.topLeft,
+                    child: const Text(
+                      '언제',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 38,
+                          color: Colors.black),
+                    ),
+                  ),
+                  Container(
+                      margin: const EdgeInsets.fromLTRB(8, 4, 0, 0),
+                      child: Image.asset('assets/splashchip.png'))
+                ],
+              ),
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            height: 56,
+            margin: const EdgeInsets.fromLTRB(20, 0, 20, 60),
+            child: TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/create');
+              },
+              style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: const Color(0xFFFA6027),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10), // <-- Radius
+                  )),
+              child: const Text("약속 만들기",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17)),
+            ),
+          ),
+        ],
       ),
     );
   }
