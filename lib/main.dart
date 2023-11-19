962c79e311b67d4d6b9a1e421ac4513549cb84e0
전해성 <seong0428@gmail.com>
2023-11-19 15:58:47 +0900

    feat: url 사용 위해 flutter_web_plugins 추가 (#6)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'when2yapp_app.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(const When2YappApp());
 }
