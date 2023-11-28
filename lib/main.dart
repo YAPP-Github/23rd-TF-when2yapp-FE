3e2d20a471657c7a6b2c05a4ceef12a92c8d4ee6
전해성 <seong0428@gmail.com>
2023-11-28 09:23:24 +0900

    feat: Firebase 추가 (#47)
    * feat: firebase_core 추가
    
    * feat: firebase 앱 등록
    
    * feat: firebase 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
+import 'firebase_options.dart';
 import 'when2yapp_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   usePathUrlStrategy();
   runApp(const When2YappApp());
 }
